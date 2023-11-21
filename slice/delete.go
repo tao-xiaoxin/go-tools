b1a761494decdbd38cff04db0cb433ff0d709653
taoxiaoxin <taoxiaoxin@111.com>
2023-11-21 22:29:37 +0800

    feat:切片删除操作实现

---
+++ slice/delete.go
@@ -0,0 +1,20 @@
+package slice
+
+import (
+	"fmt"
+	"github.com/tao-xiaoxin/go-tools/errors"
+)
+
+/*
+按照索引删除单个指定元素
+*/
+func Delete[T any](slice []T, index int) ([]T, T, error) {
+	length := len(slice)
+	if (index < 0) || (index >= length) {
+		var zero T
+		return nil, zero, errors.NewErrIndexOutOfRange(length, index)
+	}
+	fmt.Println("Element:", slice[index])
+	ele := slice[index]
+	return append(slice[:index], slice[index+1:]...), ele, nil
+}
