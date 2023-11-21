039dde037ed2e4026c0dfaa276a32b81a4995432
taoxiaoxin <taoxiaoxin@111.com>
2023-11-21 23:06:13 +0800

    feat: errors  NewErrValueNotFound 创建一个代表值不存在的错误

--- errors/error.go
+++ errors/error.go
@@ -6,3 +6,8 @@ import "fmt"
 func NewErrIndexOutOfRange(length int, index int) error {
 	return fmt.Errorf("go-tools: 下标超出范围，长度 %d, 下标 %d", length, index)
 }
+
+// NewErrValueNotFound 创建一个代表值不存在的错误
+func NewErrValueNotFound[T any](args []T, value T) error {
+	return fmt.Errorf("go-tools: %v not found in %v", value, args)
+}
