8ce85e5028ac7a98461df8fe8533a52c3c98cb72
taoxiaoxin <taoxiaoxin@111.com>
2023-11-21 23:07:51 +0800

    feat：新增切片 Remove 按照切片中的某个值删除

--- slice/remove.go
+++ slice/remove.go
@@ -1,1 +1,13 @@
 package slice
+
+import "github.com/tao-xiaoxin/go-tools/errors"
+
+// Remove 按照切片中的某个值删除
+func Remove[T comparable](slice []T, value T) ([]T, error) {
+	for i, v := range slice {
+		if v == value {
+			return append(slice[:i], slice[i+1:]...), nil
+		}
+	}
+	return nil, errors.NewErrValueNotFound(slice, value)
+}
