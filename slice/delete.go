e00f1337f8509b17200bc2e5318c322f06a7360a
haloworker <haloworker@111.com>
2024-01-10 16:34:59 +0800

    fix: 切片delete方法实现高性能

--- slice/delete.go
+++ slice/delete.go
@@ -5,14 +5,18 @@ import (
 )
 
 /*
-按照索引删除单个指定元素
+Delete 按照索引删除单个指定元素
 */
-func Delete[T any](slice []T, index int) ([]T, T, error) {
-	length := len(slice)
+func Delete[T any](src []T, index int) ([]T, error) {
+	length := len(src)
 	if (index < 0) || (index >= length) {
-		var zero T
-		return nil, zero, errors.NewErrIndexOutOfRange(length, index)
+		return nil, errors.NewErrIndexOutOfRange(length, index)
 	}
-	ele := slice[index]
-	return append(slice[:index], slice[index+1:]...), ele, nil
+	//从index位置开始，后面的元素依次往前挪1个位置
+	for i := index; i+1 < length; i++ {
+		src[i] = src[i+1]
+	}
+	//去掉最后一个重复元素
+	src = src[:length-1]
+	return src, nil
 }
